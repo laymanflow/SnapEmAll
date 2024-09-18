38aa291d648e900cd041eb75def4cf5be6c09bf5
espetrie <espetrie@syr.edu>
2024-09-18 18:39:06 -0400

    Sign in page UI

--- SnapEmAll/ContentView.swift
+++ SnapEmAll/ContentView.swift
@@ -12,7 +12,23 @@ struct ContentView: View {
     @State private var password: String = "";
     
     var body: some View{
-        
+        VStack {
+            //Sign in message
+            Text("Sign In")
+                .font(.largeTitle)
+                .padding()
+            
+            //Username text box
+            TextField("Username: ", text: $username)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            
+            //Password text box
+            TextField("Password: ", text: $password)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+        }
+        .padding()
     }
 }
 
