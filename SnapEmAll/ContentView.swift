3aff3b86ca33adc977b2599ad9a24b1b07e7f822
espetrie <espetrie@syr.edu>
2024-09-18 14:26:42 -0400

    Initial Commit

---
+++ SnapEmAll/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnapEmAll
+//
+//  Created by Ethan Petrie on 9/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username: String = "";
+    @State private var password: String = "";
+    
+    var body: some View{
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnapEmAll/SnapEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapEmAllApp.swift
+//  SnapEmAll
+//
+//  Created by Ethan Petrie on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
